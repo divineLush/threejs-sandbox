6ea7cbf828a5e23051410bd491dcd74901ac567b
divineLush <homeoflucifer@gmail.com>
2021-03-08 14:31:45 +0700

    refactor

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 (function() {
-    var scene = new THREE.Scene();
+    var scene, camera, renderer, cube;
 
-    var aspectRatio = window.innerWidth / window.innerHeight;
-    // fov, aspect ratio, near plane, far plane
-    var camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+    function calcAspectRatio() {
+        return window.innerWidth / window.innerHeight;
+    }
 
-    // antialias makes cube edges look better
-    var renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    init();
+    function init() {
+        scene = new THREE.Scene();
 
-    document.body.appendChild(renderer.domElement);
+        // fov, aspect ratio, near plane, far plane
+        camera = new THREE.PerspectiveCamera(75, calcAspectRatio(), 0.1, 1000);
+    
+        // antialias makes cube edges look better
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    
+        document.body.appendChild(renderer.domElement);
+    
+        // create cube
+        var geometry = new THREE.BoxGeometry(2, 2, 2, 2);
+        var material = new THREE.MeshBasicMaterial({ color: '#4dd080' });
+        cube = new THREE.Mesh(geometry, material);
+    
+        scene.add(cube);
+    
+        camera.position.z = 5;    
+    }
 
-    var geometry = new THREE.BoxGeometry(2, 2, 2, 2);
-    var material = new THREE.MeshBasicMaterial({ color: '#4dd080' });
-    var cube = new THREE.Mesh(geometry, material);
-
-    scene.add(cube);
-
-    camera.position.z = 5;
-
+    animate();
     function animate() {
         // create a loop that makes the renderer draw the scene
         // every time the screen is refreshed
@@ -33,5 +43,12 @@
         renderer.render(scene, camera);
     }
 
-    animate();
+    function onWindowResize() {
+        // resize renderer size on window resize
+        camera.aspect = calcAspectRatio();
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', onWindowResize, false);
 })();
